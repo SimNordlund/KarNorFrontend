66a5800bd15b2c1ee9d42c89056902e71edf39c2
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2024-09-20 16:20:00 +0200

    Testing building react app with npm build version 4

--- karnor-app/vite.config.ts
+++ karnor-app/vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.png', '**/*.PNG'],
-})
+  root: '.',  // This ensures Vite looks for `index.html` in the root directory
+  base: '/',  // Set base URL for your project, use './' if you deploy in a subdirectory
+  build: {
+    rollupOptions: {
+      input: './index.html',  // Explicitly define where the `index.html` is located
+    },
+  },
+  assetsInclude: ['**/*.png', '**/*.PNG'],  // This part is fine for including assets
+});
