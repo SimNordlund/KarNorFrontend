6ef801c9e27207fc783235b47fa5223a11835d7f
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2024-09-26 19:52:03 +0200

    fail?? did it work?

--- karnor-app/src/Home.tsx
+++ karnor-app/src/Home.tsx
@@ -1,7 +1,6 @@
 import HeroComponent from './components/HeroComponent';
 import FeatureComponent from './components/FeatureComponent';
 import FooterComponent from './components/FooterComponent';
-import HjulComponent from './components/HjulComponent';
 
 function Home() {
 
